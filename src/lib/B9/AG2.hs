f1be21e82b0020ef41565bd76ac5ad589476b6a5
Sven Heyll <sven.heyll@lindenbaum.eu>
2015-03-31 21:39:02 +0200

    Add experimental AG2 module

--- src/lib/B9/AG2.hs
+++ src/lib/B9/AG2.hs
@@ -1,10 +1,12 @@
--- | Top-Level data types for B9 build artifacts.
+-- | __EXPERIMENTAL__ dont look!
 module B9.AG2 where
 
 
 import Data.Data
+import Control.Monad.Trans.RWS.Lazy
 import Data.Monoid -- hiding ((<>))
 import Control.Applicative
+import Control.Applicative.Free
 
 import B9.DiskImages
 import B9.Vm
@@ -14,21 +16,63 @@ import B9.QCUtil
 
 import Test.QuickCheck
 
-data Artifact c = VmImage ImageDestination c
-                | VmBuild [ImageArtifact c] VmScript
+type AssemblyM m = RWST AssemblySources AssemblyTargets AssemblyState m
 
-data ArtifactContent c = Txt String
-                       | FS FSType FSResize FSSource (FileSet c)
-                       | PipeThroughCmd String c
-                       | OutputOfCmd String
-                       | DiskImage ImageType ImageResize c
-                       | LXCRun VmScript c
-                       | MBRPartition MBRBootFlag (Maybe c) (Maybe c) (Maybe c) (Maybe c)
+data AssemblySources = ASources
+data AssemblyTargets = ATargets
+data AssemblyState = AState
 
-data MBRBootFlag = NoBootablePartition
-                 | Partition1Bootable
-                 | Partition2Bootable
-                 | Partition3Bootable
-                 | Partition4Bootable
 
-data VmBuildImage c = VmBuildImage ImageDestination MountPoint c
+data XAsm x = XAddSource FilePath FilePath x
+
+data XCnt x = XSourceFile FilePath (XCntParser x)
+            | XString String x
+            | XObject [(String, x)]
+            | XList [x]
+
+type XCntParser a = String -> Either String a
+
+
+data B9Assembly next = VM_IMAGE ImageType ImageResize next
+                     | SRC ArtifactSource next
+                     | LET String String next
+                     | OUTPUT_FILE FilePath next
+                     | SHARED_IMAGE ImageResize next
+                     | FILESYSTEM FileSystemProgram next
+
+data FileSystemAssembly file next = FROM ImageSource
+                                  | ADD_FILE file next
+
+-- UC1: Vm image from a set of files
+testUC1 inFiles outFile =
+  fileTarget outFile <*> (diskImage Raw KeepSize <*> (emptyFileSystem "my_fs_label" Ext4 64 GB <> additionalFiles "/" inFiles))
+
+-- UC2: Vm Image based in shared image.
+testUC2 outFile =
+  fileTarget outFile <*> sharedImage "fedora-20-prod"
+
+sharedImage :: String ->
+
+
+-- UC3: A Vm Image modified by a custom MonadIO action
+
+
+-- data Artifact c = VmImage ImageDestination c
+--                 | VmBuild [ImageArtifact c] VmScript
+--
+--
+-- data ArtifactContent c = Txt String
+--                        | FS FSType FSResize FSSource (FileSet c)
+--                        | PipeThroughCmd String c
+--                        | OutputOfCmd String
+--                        | DiskImage ImageType ImageResize c
+--                        | LXCRun VmScript c
+--                        | MBRPartition MBRBootFlag (Maybe c) (Maybe c) (Maybe c) (Maybe c)
+--
+-- data MBRBootFlag = NoBootablePartition
+--                  | Partition1Bootable
+--                  | Partition2Bootable
+--                  | Partition3Bootable
+--                  | Partition4Bootable
+--
+-- data VmBuildImage c = VmBuildImage ImageDestination MountPoint c
