232d94db0a187b5765123511abf92ed77ef6cd58
Sven Heyll <sven.heyll@lindenbaum.eu>
2015-10-27 18:42:44 +0100

    Move all code in B9 to B9.Builder

--- src/lib/B9.hs
+++ src/lib/B9.hs
@@ -11,7 +11,7 @@
 
 -}
 
-module B9 (configure, b9_version, module X, defaultMain) where
+module B9 (b9_version, module X) where
 #if !MIN_VERSION_base(4,8,0)
 import           Control.Applicative as X
 #endif
@@ -28,27 +28,8 @@ import           Text.Printf as X (printf)
 import           Data.Version as X
 import           B9.Builder as X
 import           Paths_b9 (version)
-import qualified B9.LibVirtLXC as LibVirtLXC
-import qualified B9.DSL as Neu
 
--- | Merge 'existingConfig' with the configuration from the main b9 config
--- file. If the file does not exists, a new config file with the given
--- configuration will be written. The return value is a parser for the config
--- file. Returning the raw config file parser allows modules unkown to
--- 'B9.B9Config' to add their own values to the shared config file.
-configure :: MonadIO m => Maybe SystemPath -> B9Config -> m ConfigParser
-configure b9ConfigPath existingConfig = do
-  writeInitialB9Config b9ConfigPath existingConfig LibVirtLXC.setDefaultConfig
-  readB9Config b9ConfigPath
 
 -- | Return the cabal package version of the B9 library.
 b9_version :: Version
 b9_version = version
-
-defaultMain :: Neu.Program () -> IO Bool
-defaultMain p = do
-    (opts,vars) <- getGlobalOptsFromCLI
-    let cfgCli = cliB9Config opts
-        cfgFile = configFile opts
-    cp <- configure cfgFile cfgCli
-    runProgram (p >> return True) vars cp cfgCli
--- src/lib/B9/Builder.hs
+++ src/lib/B9/Builder.hs
@@ -2,7 +2,7 @@
 {-|
 Highest-level build functions and and B9-re-exports.
 -}
-module B9.Builder (buildArtifacts, runProgram, runIoProgram, module X) where
+module B9.Builder (buildArtifacts, runProgram, runIoProgram, defaultMain, configure, module X) where
 import B9.ArtifactGenerator as X
 import B9.ArtifactGeneratorImpl as X
 import B9.B9Config as X
@@ -28,6 +28,31 @@ import qualified B9.DSL as Neu
 import qualified B9.DSL.Interpreter as Neu
 import qualified B9.B9IO as Neu
 import qualified B9.B9IOImpl as Neu
+import qualified B9.LibVirtLXC as LibVirtLXC
+
+-- | Use this in your 'B9' script to run a 'Program'.
+defaultMain :: Neu.Program () -> IO ()
+defaultMain = void . runProgramWithConfigAndCliArgs
+
+-- | Execute a 'Program' in the 'B9' monad; parse all b9 command line options
+-- and read the b9 configuration file.
+runProgramWithConfigAndCliArgs :: Neu.Program () -> IO Bool
+runProgramWithConfigAndCliArgs p = do
+    (opts,vars) <- getGlobalOptsFromCLI
+    let cfgCli = cliB9Config opts
+        cfgFile = configFile opts
+    cp <- configure cfgFile cfgCli
+    runProgram (p >> return True) vars cp cfgCli
+
+-- | Merge 'existingConfig' with the configuration from the main b9 config
+-- file. If the file does not exists, a new config file with the given
+-- configuration will be written. The return value is a parser for the config
+-- file. Returning the raw config file parser allows modules unkown to
+-- 'B9.B9Config' to add their own values to the shared config file.
+configure :: MonadIO m => Maybe SystemPath -> B9Config -> m ConfigParser
+configure b9ConfigPath existingConfig = do
+  writeInitialB9Config b9ConfigPath existingConfig LibVirtLXC.setDefaultConfig
+  readB9Config b9ConfigPath
 
 runProgram :: Neu.Program Bool
            -> Environment
