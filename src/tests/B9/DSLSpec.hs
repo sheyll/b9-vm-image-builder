0c8cf6484cdc890f53b0aa569e203be13ebd97a2
Sven Heyll <sven.heyll@lindenbaum.eu>
2015-12-12 23:33:31 +0100

    Add DSL unit-test stub

---
+++ src/tests/B9/DSLSpec.hs
@@ -0,0 +1,19 @@
+module B9.B9IOSpec (spec) where
+
+import           B9.B9IO
+import           B9.CommonTypes
+import           B9.Content
+import           B9.DiskImages
+import           B9.FileSystems
+import           B9.Logging
+import           B9.PartitionTable
+import           B9.Repository
+import           Data.Default
+import           Data.List
+import           System.FilePath
+import           Test.Hspec
+import           Test.QuickCheck
+import           Text.Printf
+
+spec :: Spec
+spec = undefined
