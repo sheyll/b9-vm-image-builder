23a456490536970445344e983c37a08c0244d64c
Sven Heyll <sven.heyll@lindenbaum.eu>
2019-02-22 22:50:01 +0100

    Generalize ContentGenerator to ContentGeneratorT

--- src/lib/B9/Content/Generator.hs
+++ src/lib/B9/Content/Generator.hs
@@ -1,8 +1,9 @@
 module B9.Content.Generator
-  ( ContentGenerator(MkContentGenerator)
+  ( ContentGeneratorT(MkContentGeneratorT)
+  , ContentGenerator
   , ByteStringGenerator
   , renderContentGenerator
-  , CanRender (..)
+  , ToContentGenerator (..)
   ) where
 
 import B9.B9Monad (B9)
@@ -11,23 +12,29 @@ import Control.Applicative (Alternative)
 import Control.Monad.Reader
 import Data.ByteString.Lazy as Lazy
 
-newtype ContentGenerator a = MkContentGenerator
-  { getContentGenerator :: EnvironmentReaderT B9 a
-  } deriving (Alternative, MonadReader Environment, MonadIO, Monad, Applicative, Functor)
+newtype ContentGeneratorT m a = MkContentGeneratorT
+  { getContentGenerator :: EnvironmentReaderT m a
+  } deriving (MonadTrans, Alternative, MonadReader Environment, MonadIO, Monad, Applicative, Functor)
+
+type ContentGenerator a = ContentGeneratorT B9 a
 
 type ByteStringGenerator = ContentGenerator Lazy.ByteString
 
-instance Semigroup a => Semigroup (ContentGenerator a) where
+instance (Monad m, Semigroup a) => Semigroup (ContentGeneratorT m a) where
   l <> r = (<>) <$> l <*> r
 
-instance Monoid a => Monoid (ContentGenerator a) where
+instance (Monad m, Monoid a) => Monoid (ContentGeneratorT m a) where
   mempty = pure mempty
 
+-- | Types whose values can be turned into a 'ContentGenerator'
+--
+-- @since 0.5.62
+class ToContentGenerator c where
+    toContentGenerator :: c -> ByteStringGenerator
 
--- | Types of values that can be /rendered/ into a 'ByteString'
-class CanRender c  where
-    render
-        :: c -> EnvironmentReaderT B9 Lazy.ByteString
-
+-- | Execute a 'ContentGenerator'.
+-- The 'Environment' contains the bindings of any string template variables.
+--
+-- @since 0.5.62
 renderContentGenerator :: Environment -> ContentGenerator a -> B9 a
 renderContentGenerator e = runEnvironmentReaderT e . getContentGenerator
