45b0c9955a6cd155b3722c1d2a2650710d96234a
Sven Heyll <sven.heyll@lindenbaum.eu>
2020-07-01 04:40:07 +0200

    Refactor DiskImageBuilder and RepositoryIO

--- src/lib/B9/RepositoryIO.hs
+++ src/lib/B9/RepositoryIO.hs
@@ -1,27 +1,46 @@
 -- | Effectful functions executing shared image respository operations.
 --    See "B9.Repository"
 module B9.RepositoryIO
-  ( repoSearch,
-    pushToRepo,
-    pullFromRepo,
+  ( repoSearch, -- internal 
+    pushToRepo, -- internal
+    pullFromRepo, -- internal
     pullGlob,
     Repository (..),
     toRemoteRepository,
     FilePathGlob (..),
+    lookupSharedImages,
+    getSharedImages,
+    getSharedImagesCacheDir,
+    getSelectedRepos,
+    pullRemoteRepos,
+    pullLatestImage,
+    cleanOldSharedImageRevisionsFromCache,
+    pushToSelectedRepo,  
+    pushSharedImageLatestVersion,
+    getLatestImageByName,
   )
 where
 
-import B9.B9Config (getRemoteRepos)
+import Data.Maybe
+import Data.Function (on)
+import B9.B9Config (B9ConfigReader, getRemoteRepos, getConfig, maxLocalSharedImageRevisions)
 import B9.B9Exec
+import B9.B9Error
 import B9.B9Logging
 import B9.Repository
+import B9.DiskImages
 import Control.Eff
+import Control.Exception
+import Control.Lens ((^.))
 import Control.Monad.IO.Class
+import Control.Monad (when, forM_)
 import Data.List
+import GHC.Stack
 import System.Directory
 import System.FilePath
-import System.IO.B9Extras (ensureDir)
+import System.IO.B9Extras (consult, ensureDir)
 import Text.Printf (printf)
+import Text.Show.Pretty (ppShow)
 
 data Repository
   = Cache
@@ -161,3 +180,180 @@ sshOpts (RemoteRepo _repoId _rootDir (SshPrivKey key) (SshRemoteHost (_host, por
       "-o",
       "IdentityFile=" ++ key
     ]
+
+
+-- | Find shared images and the associated repos from two predicates. The result
+-- is the concatenated result of the sorted shared images satisfying 'imgPred'.
+lookupSharedImages :: 
+  (HasCallStack, Lifted IO e, CommandIO e, Member RepoCacheReader e) =>
+  (Repository -> Bool) ->
+  (SharedImage -> Bool) ->
+  Eff e [(Repository, SharedImage)]
+lookupSharedImages repoPred imgPred = do
+  xs <- getSharedImages
+  let rs = [(r, s) | (r, ss) <- xs, s <- ss]
+      matchingRepo = filter (repoPred . fst) rs
+      matchingImg = filter (imgPred . snd) matchingRepo
+      sorted = sortBy (compare `on` snd) matchingImg
+  return (mconcat (pure <$> sorted))
+
+-- | Return a list of all existing sharedImages from cached repositories.
+getSharedImages ::
+  (HasCallStack, CommandIO e, Lifted IO e, Member RepoCacheReader e) =>
+  Eff e [(Repository, [SharedImage])]
+getSharedImages = do
+  reposAndFiles <-
+    repoSearch
+      sharedImagesRootDirectory
+      (FileExtension sharedImageFileExtension)
+  mapM
+    (\(repo, files) -> (repo,) . catMaybes <$> mapM consult' files)
+    reposAndFiles
+  where
+    consult' f = do
+      r <- liftIO (try (consult f))
+      case r of
+        Left (e :: SomeException) -> do
+          dbgL
+            ( printf
+                "Failed to load shared image meta-data from '%s': '%s'"
+                (takeFileName f)
+                (show e)
+            )
+          dbgL (printf "Removing bad meta-data file '%s'" f)
+          liftIO (removeFile f)
+          return Nothing
+        Right c -> return (Just c)
+
+-- | Pull metadata files from all remote repositories.
+pullRemoteRepos :: 
+  (HasCallStack, Lifted IO e, CommandIO e, '[SelectedRemoteRepoReader, RepoCacheReader] <:: e) => 
+  Eff e ()
+pullRemoteRepos = do
+  repos <- getSelectedRepos
+  mapM_ dl repos
+  where
+    dl =
+      pullGlob
+        sharedImagesRootDirectory
+        (FileExtension sharedImageFileExtension)
+
+-- | Pull the latest version of an image, either from the selected remote
+-- repo or from the repo that has the latest version.
+pullLatestImage :: 
+  (HasCallStack, Lifted IO e, CommandIO e, '[RepoCacheReader,SelectedRemoteRepoReader] <:: e) => 
+  SharedImageName -> Eff e (Maybe SharedImageBuildId)
+pullLatestImage name@(SharedImageName dbgName) = do
+  repos <- getSelectedRepos
+  let repoPredicate Cache = False
+      repoPredicate (Remote repoId) = repoId `elem` repoIds
+      repoIds = map remoteRepoRepoId repos
+      hasName sharedImage = name == sharedImageName sharedImage
+  candidates <- lookupSharedImages repoPredicate hasName
+  let (Remote repoId, image) = last candidates
+  if null candidates
+    then do
+      errorL
+        ( printf
+            "No shared image named '%s' on these remote repositories: '%s'"
+            dbgName
+            (ppShow repoIds)
+        )
+      return Nothing
+    else do
+      dbgL (printf "PULLING SHARED IMAGE: '%s'" (ppShow image))
+      cacheDir <- getSharedImagesCacheDir
+      let (Image imgFile' _imgType _fs) = sharedImageImage image
+          cachedImgFile = cacheDir </> imgFile'
+          cachedInfoFile = cacheDir </> sharedImageFileName image
+          repoImgFile = sharedImagesRootDirectory </> imgFile'
+          repoInfoFile = sharedImagesRootDirectory </> sharedImageFileName image
+          repo = fromJust (lookupRemoteRepo repos repoId)
+      pullFromRepo repo repoImgFile cachedImgFile
+      pullFromRepo repo repoInfoFile cachedInfoFile
+      infoL (printf "PULLED '%s' FROM '%s'" dbgName repoId)
+      cleanOldSharedImageRevisionsFromCache name
+      return (Just (sharedImageBuildId image))
+
+-- | Return the 'Image' of the latest version of a shared image named 'name'
+-- from the local cache.
+getLatestImageByName :: 
+  (HasCallStack, Lifted IO e, CommandIO e, Member RepoCacheReader e) => 
+  SharedImageName -> Eff e (Maybe Image)
+getLatestImageByName name = do
+  sharedImage <- getSharedImageVersionsFromCache name
+  cacheDir <- getSharedImagesCacheDir
+  let image = changeImageDirectory cacheDir . sharedImageImage <$> listToMaybe sharedImage
+  case image of
+    Just i -> dbgL (printf "USING SHARED SOURCE IMAGE '%s'" (show i))
+    Nothing -> errorL (printf "SOURCE IMAGE '%s' NOT FOUND" (show name))
+  return image
+
+-- | Return the versions of a shared image named 'name' from the local cache.
+--
+-- @since 1.1.0
+getSharedImageVersionsFromCache ::
+  (Member RepoCacheReader e, Lifted IO e, CommandIO e) =>
+  SharedImageName -> Eff e [SharedImage]
+getSharedImageVersionsFromCache name = do
+  imgs <- lookupSharedImages (== Cache) ((== name) . sharedImageName)
+  return (reverse (sort (map snd imgs)))
+
+-- | Depending on the 'maxLocalSharedImageRevisions' 'B9Config' settings either
+-- do nothing or delete all but the configured number of most recent shared
+-- images with the given name from the local cache.
+cleanOldSharedImageRevisionsFromCache :: 
+  ( '[B9ConfigReader, RepoCacheReader] <:: e, Lifted IO e, CommandIO e) =>
+  SharedImageName -> Eff e ()
+cleanOldSharedImageRevisionsFromCache _sn = do
+  b9Cfg <- getConfig
+  forM_ (b9Cfg ^. maxLocalSharedImageRevisions) $ \_maxRevisions -> do
+    error "TODO"    
+
+-- | Publish the latest version of a shared image identified by name to the
+-- selected repository from the cache.
+pushSharedImageLatestVersion ::
+ (Lifted IO e, CommandIO e, '[SelectedRemoteRepoReader, RepoCacheReader, ExcB9] <:: e) =>
+ SharedImageName -> Eff e ()
+pushSharedImageLatestVersion name@(SharedImageName imgName) =
+  getSharedImageVersionsFromCache name
+    >>= maybe
+      (errorExitL (printf "Nothing found for %s." (show imgName)))
+      ( \sharedImage -> do
+          dbgL (printf "PUSHING '%s'" (ppShow sharedImage))
+          pushToSelectedRepo sharedImage
+          infoL (printf "PUSHED '%s'" imgName)
+      ) . listToMaybe
+
+-- | Upload a shared image from the cache to a selected remote repository
+pushToSelectedRepo :: 
+  (Lifted IO e, CommandIO e, '[B9ConfigReader, LoggerReader, RepoCacheReader, SelectedRemoteRepoReader] <:: e) =>
+  SharedImage -> Eff e ()
+pushToSelectedRepo i = do
+  c <- getSharedImagesCacheDir
+  MkSelectedRemoteRepo r <- getSelectedRemoteRepo
+  when (isJust r) $ do
+    let (Image imgFile' _imgType _imgFS) = sharedImageImage i
+        cachedImgFile = c </> imgFile'
+        cachedInfoFile = c </> sharedImageFileName i
+        repoImgFile = sharedImagesRootDirectory </> imgFile'
+        repoInfoFile = sharedImagesRootDirectory </> sharedImageFileName i
+    pushToRepo (fromJust r) cachedImgFile repoImgFile
+    pushToRepo (fromJust r) cachedInfoFile repoInfoFile
+
+
+-- | Return either all remote repos or just the single selected repo.
+getSelectedRepos :: '[B9ConfigReader, SelectedRemoteRepoReader] <:: e => Eff e [RemoteRepo]
+getSelectedRepos = do
+  allRepos <- getRemoteRepos
+  MkSelectedRemoteRepo selectedRepo <- getSelectedRemoteRepo
+  let repos = maybe allRepos return selectedRepo -- 'Maybe' a repo
+  return repos
+
+-- | Return the path to the sub directory in the cache that contains files of
+-- shared images.
+getSharedImagesCacheDir :: '[RepoCacheReader] <:: e => Eff e FilePath
+getSharedImagesCacheDir = do
+  cacheDir <- localRepoDir <$> getRepoCache
+  return (cacheDir </> sharedImagesRootDirectory)
+
